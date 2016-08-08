3472b5dd56eb61449379b20f57d1a59aba12ae20
jaygradon <jaygradon@outlook.com>
2016-08-09 02:23:30 +1200

    MessageProc working with demo_simple

--- process_message_system.py
+++ process_message_system.py
@@ -1,69 +1,65 @@
-
+import threading
 import os
 import pickle
+import queue
+import multiprocessing
+import time
 
 ANY = 'any'
 
 class MessageProc:
 
-    # Set up MessageProc?
     def main(self):
-#        print("printing " + ANY)
-#        print ("MessageProc: main")
         # Open a file for message queue
-        self.mypipe = "/tmp/pipe" + str(os.getpid()) + ".fifo"
-        if not os.path.exists(self.mypipe):
-            os.mkfifo(self.mypipe)
-        
+        pipename = "/tmp/pipe" + str(os.getpid()) + ".fifo"
+        if not os.path.exists(pipename):
+            os.mkfifo(pipename)
+        self.messageQueue = queue.Queue(maxsize=0)
+        self.read_condition = threading.Condition()
+        readThread = threading.Thread(target=self.readPipeToQueue, daemon=True)
+        readThread.start()
+    
+    # Sourced from lectures
+    def readPipeToQueue(self):
+        with open("/tmp/pipe" + str(os.getpid()) + ".fifo", 'rb') as fifo:
+            while True:
+                try:
+                    message = pickle.load(fifo)
+                    with self.read_condition:
+                        self.messageQueue.put(message)
+                        self.read_condition.notify()
+                except EOFError:
+                    time.sleep(0.02)
+
 
     # Send data to process with process id pid
     def give(self, pid, label, *values):
-#        print ("MessageProc: give:", label, " to pid: ", pid)
-        # Print to message queue file with pickle(?) and/or pipe
-        pipe_name = "/tmp/pipe" + str(pid) + ".fifo"
-        fifo = open(pipe_name, 'wb')
-        data = pickle.dumps([label, *values])
-#        print ("give data : " + str(data))
-#        print ("give label: " + str(label))
-#        print ("give value: " + str(values))
-        fifo.write(data)
-        fifo.close()
-    
-    # Receive data sent to process id of self?
+        time.sleep(0.1)
+        with open("/tmp/pipe" + str(pid) + ".fifo", 'wb') as fifo:
+            pickle.dump((label, values), fifo)
+        
+    # Receive data sent to self
     def receive(self, *messages):
-#        print ("MessageProc: receive")
-        while True:
-            fifo = open(self.mypipe, 'rb')
-            # TODO read last line first
-            for line in fifo:
-#                print ("rece data : " + str(line))
-                label, *values = pickle.loads(line)
-#                print ("rece label: " + str(label))
-#                print ("rece value: " + str(values))
-                for message in messages:
-                    if(label == message.label or message.label == ANY):
-                        if (message.guard()):
-                            # TODO delete read line here
-                            return message.action(*values)
-            fifo.close()
-        return 0
-
-
+         with self.read_condition:
+            self.read_condition.wait()
+            label, values = self.messageQueue.get(True)
+            for message in messages:
+                if label == message.label or message.label == ANY:
+                    if message.guard():
+                        return message.action(*values)
+            self.messageQueue.put((label, values))
+            
     # Creates new process
     def start(self):
-#        print ("MessageProc: start")
         pid = os.fork()
         if pid == 0:
-#            print ("\nI am child")
             self.main()
         else:
-#            print ("I am parent")
             return pid
 
 class Message:
     # Creates a Message object
     def __init__(self, label, guard=lambda: True, action=lambda *args: None):
-#        print ("Message: Initialized")
         self.label = label
         self.guard = guard
         self.action = action
@@ -74,17 +70,3 @@ class TimeOut:
         print ("TimeOut delay: ", delay)
         self.delay = delay
         self.action = action
-
-
-# Provided code
-# import os
-#
-# def main(*args)
-#   print ('I am a process' os.getpid())
-#   print ('I was passed these arguments', *args)
-#
-# pid = os.fork()
-# if pid == 0:
-#   main('hi', -1, list(range(5)))
-# else:
-#   print('I am a process', os.getpid())

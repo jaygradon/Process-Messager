bc82fdbab19938498cc335d725e712aeea392347
jaygradon <jaygradon@outlook.com>
2016-08-14 17:28:25 +1200

    Working second thread, attempt at demo_timeout

--- process_message_system.py
+++ process_message_system.py
@@ -9,57 +9,73 @@ ANY = 'any'
 
 class MessageProc:
 
+    def __init__(self):
+        self.give_pipes = {}
+        self.message_queue = queue.Queue(maxsize=0)
+        self.message_list = []
+        self.arrived_condition = threading.Condition()
+        self.read_thread = None
+
+
     def main(self):
         # Open a file for message queue
-        pipename = "/tmp/pipe" + str(os.getpid()) + ".fifo"
-        if not os.path.exists(pipename):
-            os.mkfifo(pipename)
-        self.messageLock = threading.Lock()
-        self.messageList = queue.Queue(maxsize=0)
-        self.read_condition = threading.Condition()
-        readThread = threading.Thread(target=self.readPipeToQueue, daemon=True)
-        readThread.start()
-        self.givePipes = {}
+        self.pipe_name = "/tmp/pipe" + str(os.getpid()) + ".fifo"
+        if not os.path.exists(self.pipe_name):
+            os.mkfifo(self.pipe_name)
         
-    
+
+
     # Sourced from lectures
-    def readPipeToQueue(self):
-        with open("/tmp/pipe" + str(os.getpid()) + ".fifo", 'rb') as fifo:
+    def read_pipe(self):
+        with open(self.pipe_name, 'rb') as pipe_rd:
             while True:
                 try:
-                    label, values = pickle.load(fifo)
-                    self.messageList.put((label, values))
+                    label, values = pickle.load(pipe_rd)
+                    with self.arrived_condition:
+                        self.message_queue.put((label, values))
+                        print("put", label, values)
+                        self.arrived_condition.notify()
                 except EOFError:
+                    # print("EOF")
                     time.sleep(0.01)
 
 
-    # Send data to process with process id pid
     def give(self, pid, label, *values):
-        if pid in self.givePipes:
-            fifo = self.givePipes[pid]
-        else:
-            fifo = open("/tmp/pipe" + str(pid) + ".fifo", 'wb')
-            self.givePipes[pid] = fifo
-        pickle.dump((label, values), fifo)
-        
+        if pid not in self.give_pipes:
+        	self.give_pipes[pid] = open("/tmp/pipe" + str(pid) + ".fifo", 'wb')
+        print("gave", label, values)
+        pickle.dump((label, values), self.give_pipes[pid])
+
     
-    def processReceive(self, label, *values):
-        while True:
-            messages = self.messages
-            for message in self.messages:
-                if label == message.label or message.label == ANY:
-                    if message.guard():
-                        return message.action(*values)
-
-
-    # Receive data sent to self
     def receive(self, *messages):
-        self.messages = messages
-        label, values = self.messageList.get()
-        processReceiveDaemon = multiprocessing.Process(target=self.processReceive, args=(label,*values,))
-        processReceiveDaemon.daemon = True
-        processReceiveDaemon.start()
-            
+        timeout = TimeOut(None, action=lambda: None)
+        if self.read_thread is None:
+            self.read_thread = threading.Thread(target=self.read_pipe, daemon=True)
+            self.read_thread.start()
+        for message in messages:
+            if isinstance(message, TimeOut):
+                timeout = message
+                print("found timeout")
+                break
+        while True:
+            for i in range(len(self.message_list)):
+                label, values = self.message_list[i]
+                for message in messages:
+                    if label == message.label or message.label == ANY:
+                        if message.guard():
+                            print("deleting",label)
+                            del self.message_list[i]
+                            return message.action(*values)
+            if self.message_queue.empty():
+                with self.arrived_condition:
+                    if not self.arrived_condition.wait(timeout=timeout.delay):
+                        print("timed out")
+                        return timeout.action()
+            label, values = self.message_queue.get()
+            print("got",label)
+            self.message_list.append((label , values))
+        
+
     # Creates new process
     def start(self):
         pid = os.fork()
@@ -77,7 +93,6 @@ class Message:
 
 class TimeOut:
     # Creates a TimeOut object
-    def __init__(self, delay, action= lambda: None):
-        print ("TimeOut delay: ", delay)
+    def __init__(self, delay, action=lambda: None):
         self.delay = delay
         self.action = action
